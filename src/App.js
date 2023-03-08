612570fffa89e1b1cefbe7b2702ecb6eac6ce795
Kathiravan Arjunan <93112145+kat369@users.noreply.github.com>
2023-03-08 15:53:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import {  useState } from 'react';
+import { io } from 'socket.io-client';
+import './App.css';
+import Chat from './Chat';
+
+const socket =io.connect("https://chatappbackendserver.onrender.com")
+
+function App() {
+const [userName,setUserName] =useState("");
+const [roomId,setRoomId] = useState("");
+const [showChat, setShowChat] = useState(false);
+
+let joinChat=()=>{
+  if(userName !==""&&roomId!==""){
+   
+    socket.emit("join_room",roomId)
+ 
+    setShowChat(true)
+  }
+  else{
+    alert("Enter the name/roomId...")
+  }
+
+}
+  
+  return (
+    <div className="App">
+  {
+    !showChat ?    (<div className='joinChatContainer'>
+    <h3 className="top">Join the Chat Room</h3>
+<input type="text" onChange={(e)=>{setUserName(e.target.value)}} placeholder='Enter the User Name'/>
+<input type ="text" onChange={(e)=>{setRoomId(e.target.value)}} placeholder='Enter the Room ID' />
+<button onClick={joinChat}>Join a room</button>
+    </div>):<Chat socket={socket} userName={userName} room={roomId} />
+  }
+
+
+    </div>
+  );
+}
+
+export default App;
